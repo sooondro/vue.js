c41bff43f6a9e6e2046e1e23f4848dbbcac7930f
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-04 22:50:26 +0100

    Data binding with attributes (v-bind:href, v-bind:value and v-html)

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Sandro',
-    job: 'Student'
+    job: 'Student',
+    website: 'https://www.facebook.com/sandro.blavicki.9',
+    websiteTag: '<a href="https://www.facebook.com/sandro.blavicki.9">Sandros website</a>'
   },
   methods: {
     greet: function(time){
