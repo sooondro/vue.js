011a25ef37a6188192afb6c60da8c14f2c4164fd
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-09 19:33:58 +0100

    Two way data binding - v-model="sth."

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 new Vue({
   el: '#vue-app',
   data: {
-
+    name: '',
+    age: ''
   },
   methods: {
-    logName: function(){
-      console.log('You entered your name');
-    },
-    logAge: function(){
-      console.log('You entered your age');
-    }
+
   }
 });
