df11e1cf8352bff0795db01aacd18a44a0f3c65c
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-09 19:25:58 +0100

    Keyboard events - keyup, modifiers - keyup.alt.enter

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 new Vue({
   el: '#vue-app',
   data: {
-    age: 25,
-    x: 0,
-    y: 0
+
   },
   methods: {
-    add: function(value) {
-      this.age += value;
+    logName: function(){
+      console.log('You entered your name');
     },
-    subtract: function(value) {
-      this.age -= value;
-    },
-    updateXY: function(event){
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    click: function(){
-      alert('you clicked me');
+    logAge: function(){
+      console.log('You entered your age');
     }
   }
 });
