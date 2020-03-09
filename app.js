6b018c0c0ce60e38aeac131eebb4bd90bc6580ba
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-09 20:44:08 +0100

    Looping with v-for

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 new Vue({
   el: '#vue-app',
   data: {
-    error: false,
-    success: false
+    name: 'Sandro',
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [
+      {name: 'Marko', age: 21},
+      {name: 'Ivica', age: 23},
+      {name: 'Daca', age: 22}
+    ]
   },
   methods: {
 
