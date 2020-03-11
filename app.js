7f31bd2be7099b4705321a146b0ba8fc1f79c15a
Sandro Blavicki <sandro.blavicki@gmail.com>
2020-03-11 12:19:08 +0100

    Refs

--- app.js
+++ app.js
@@ -1,37 +1,14 @@
-Vue.component('greeting',{
-  template: '<p>I am reusable {{ name }} . <button v-on:click="changeName">Change Name</button></p>',
-  data: function(){
-    return {
-      name: 'Sandro',
-    }
-  },
-  methods: {
-    changeName: function(){
-      this.name = 'Sandy';
-    }
-  }
-})
 
-var one = new Vue({
+new Vue({
   el: '#vue-app-one',
   data: {
-
+    output: 'Your fav food'
   },
   methods: {
-
-  },
-  computed: {
-
-  }
-});
-
-var two = new Vue({
-  el: '#vue-app-two',
-  data: {
-
-  },
-  methods: {
-
+    readRefs: function(){
+      console.log(this.$refs.test.innerText);
+      this.output = this.$refs.input.value;
+    }
   },
   computed: {
 
